4f24a3d05d9e4f014f5fd14a1299d364840d4b34
Shashi Shekhar <shashiyadav7629518@gmail.com>
2026-03-09 01:27:31 +0530

    implementing authentication

--- tracker/urls.py
+++ tracker/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from tracker.views import index, deleteTransaction
+from tracker.views import index, deleteTransaction, registeration, login_page, logout_page
 
 
 urlpatterns = [
     path('', index, name = "index"),
-    path('delete-transaction/<uuid>/', deleteTransaction, name = "deleteTransaction")
+    path('delete-transaction/<uuid>/', deleteTransaction, name = "deleteTransaction"),
+    path('registeration/', registeration),
+    path('login/', login_page, name="login_page"),
+    path('logout/', logout_page, name="logout_page")
 ]
--- tracker/views.py
+++ tracker/views.py
@@ -1,10 +1,85 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from tracker.models import Transaction
-from django.db.models import Sum
+from django.db.models import Sum, Q
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+
 # Create your views here.
 
+def registeration(request):
+    if request.method == 'POST':
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        user_obj = User.objects.filter(
+            Q(email = email) | Q(username = username) )
+        if user_obj.exists():
+            messages.error(request, "Error: username or Email alreday exists!")
+            return redirect("/registeration/")
+        
+        user_obj = User.objects.create(
+            first_name = first_name,
+            last_name = last_name,
+            username = username,
+            email = email
+        )
+
+        user_obj.set_password(password)
+        user_obj.save()
+        messages.error(request, "Success: Account created")
+        return redirect('/registeration/')
+
+
+
+        
+    return render(request,'registeration.html')
+
+
+def login_page(request):
+    
+    if request.method == "post":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user_obj = User.objects.filter(
+            username = username
+            )
+    
+        if not user_obj.exists():
+            messages.error(request, "Error: username doesnot exist!")
+            return redirect("/login/")
+    
+        user_obj = authenticate(username=username, password=password)
+
+        if not user_obj:
+            messages.error(request, "Error: Invalid credentials")
+            return redirect("/login/")   
+    
+    
+        login(request, user_obj)
+        return redirect("/")
+    
+    return render(request,'login.html')
+
+    
+
+    # if user_obj.exists():
+    #         messages.error(request, "Error: username or Email alreday exists!")
+    #         return redirect("/registeration/")
+
+
+
+def logout_page(request):
+    logout(request)
+    messages.error(request, "logout successful!")
+    return redirect("/login/")
+
+
 def index(request):
+    print(request.user)
     if request.method == "POST":
         description = request.POST.get('description')
         amount  = request.POST.get('amount')
